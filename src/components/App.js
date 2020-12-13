494c780986238fbd2c3e77c491ed4fe930a745a1
devkiratu <dev.kiratu@gmail.com>
2020-12-13 21:58:54 +0300

    add required setup to have the React app up and running

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../style.css";
+
+export default function App() {
+	return (
+		<div>
+			<h1 className="title">Hello React</h1>
+			<p>You can start your react App from here. Happy Coding!!</p>
+		</div>
+	);
+}
